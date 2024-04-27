83b6870a7da9cadefd75cd4b4d110c5733490e2d
hazadus <hazadus7@gmail.com>
2024-04-27 17:27:32 +0300

    feat: Add services list to public user profile.

--- src/users/selectors.py
+++ src/users/selectors.py
@@ -2,4 +2,8 @@ from users.models import CustomUser
 
 
 def get_user_by_username(username: str) -> CustomUser | None:
-    return CustomUser.objects.filter(username=username).first()
+    return (
+        CustomUser.objects.filter(username=username)
+        .prefetch_related("services", "services__category")
+        .first()
+    )
