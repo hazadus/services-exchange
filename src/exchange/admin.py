82c5c4e97d574ffcba611d0b2bf3958963738136
hazadus <hazadus7@gmail.com>
2024-05-02 17:33:44 +0300

    feat: Implement Chat and Message models.

--- src/exchange/admin.py
+++ src/exchange/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from exchange.models import Category
+from exchange.models import Category, Chat, Message
 
 
 @admin.register(Category)
@@ -13,4 +13,33 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = [
         "id",
         "parent",
+    ]
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    model = Chat
+    list_display = [
+        "id",
+        "created",
+        "topic",
+    ]
+    ordering = [
+        "-created",
+    ]
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = [
+        "id",
+        "chat",
+        "sender",
+        "recipient",
+        "is_read",
+        "created",
+    ]
+    ordering = [
+        "-created",
     ]
--- src/exchange/models.py
+++ src/exchange/models.py
@@ -1,4 +1,7 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from users.models import CustomUser
 
 
 class Category(models.Model):
@@ -19,9 +22,6 @@ class Category(models.Model):
 
     class Meta:
         ordering = ["title"]
-        indexes = [
-            models.Index(fields=["title"]),
-        ]
         verbose_name = "категория"
         verbose_name_plural = "категории"
 
@@ -32,3 +32,112 @@ class Category(models.Model):
             title = f"{parent.title} / {title}"
             parent = parent.parent
         return title
+
+
+class Chat(models.Model):
+    """Чат объединяет сообщения и topic – модель БД, с которой связан чат (заказ, проект...)"""
+
+    topic_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name="topic_obj",
+        on_delete=models.CASCADE,
+    )
+    topic_id = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
+    topic = GenericForeignKey(
+        ct_field="topic_ct",
+        fk_field="topic_id",
+    )
+    latest_message = models.OneToOneField(
+        to="Message",
+        verbose_name="Последнее сообщение",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="latest_in_chat",
+    )
+    created = models.DateTimeField(
+        verbose_name="создан",
+        auto_now_add=True,
+    )
+    updated = models.DateTimeField(
+        verbose_name="изменен",
+        auto_now=True,
+    )
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "чат"
+        verbose_name_plural = "чаты"
+
+    def __str__(self):
+        return f"Чат {self.pk}"
+
+
+class Message(models.Model):
+    """Сообщение в чате."""
+
+    chat = models.ForeignKey(
+        to=Chat,
+        verbose_name="чат",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="messages",
+    )
+    sender = models.ForeignKey(
+        to=CustomUser,
+        verbose_name="отправитель",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="messages_sent",
+    )
+    recipient = models.ForeignKey(
+        to=CustomUser,
+        verbose_name="получатель",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="messages_received",
+    )
+    text = models.TextField(
+        verbose_name="Текст сообщения",
+        max_length=1500,
+        blank=False,
+        null=False,
+    )
+    file = models.FileField(
+        verbose_name="вложение",
+        help_text="Файл, прилагаемый к сообщению.",
+        upload_to="attachments/",
+        blank=True,
+        null=True,
+    )
+    is_read = models.BooleanField(
+        verbose_name="прочитано",
+        help_text="Сообщение прочитано получателем.",
+        default=False,
+    )
+    created = models.DateTimeField(
+        verbose_name="создано",
+        auto_now_add=True,
+    )
+    updated = models.DateTimeField(
+        verbose_name="изменено",
+        auto_now=True,
+    )
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "cообщение"
+        verbose_name_plural = "сообщения"
+
+    def __str__(self):
+        return (
+            f"Сообщение {self.pk} от {self.sender.username} к {self.recipient.username}"
+        )
